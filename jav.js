7a0700779d3ec48f29d6d42163316b1a946138e9
Asif Raza <99474660+asifeth@users.noreply.github.com>
2022-06-14 20:46:14 +0530

    Update jav.js

--- jav.js
+++ jav.js
@@ -4,8 +4,19 @@ let time = number*60;
 setInterval(updatecountdown, 1000);
 function updatecountdown()
 {
+    
     const minutes = Math.floor(time/60);
     const seconds = time%60;
+    if(time>0)
+    {
+        
     document.getElementById('countdown').innerHTML=minutes + ':' + seconds;
-    time--;
+   
+    
+}
+else
+{
+    document.getElementById('countdown').innerHTML="Ended";
+}
+time--;
 }
